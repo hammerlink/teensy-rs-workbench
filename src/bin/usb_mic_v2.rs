9a1f0318cbe06bfb1c428037ee29e10c7aef1bbb
hendrik <hendrik.hamerlinck@hammernet.be>
2025-02-10 12:12:19 +0100

    update mic v2 with interrupt to send 125microsecond intervalled data

--- src/bin/usb_mic_v2.rs
+++ src/bin/usb_mic_v2.rs
@@ -28,12 +28,17 @@ mod app {
     /// Matches whatever is in imxrt-log.
     const VID_PID: UsbVidPid = UsbVidPid(0x5824, 0x27dd);
 
-    const SINETAB: [i16; 48] = [
-        0i16, 4276, 8480, 12539, 16383, 19947, 23169, 25995, 28377, 30272, 31650, 32486, 32767,
-        32486, 31650, 30272, 28377, 25995, 23169, 19947, 16383, 12539, 8480, 4276, 0, -4276, -8480,
-        -12539, -16383, -19947, -23169, -25995, -28377, -30272, -31650, -32486, -32767, -32486,
-        -31650, -30272, -28377, -25995, -23169, -19947, -16383, -12539, -8480, -4276,
-    ];
+    const SINETAB: [u8; 96] = [
+    0,   0, 180,  16,  32,  33, 251,  48, 255,  63, 235,  77,
+  129,  90, 139, 101, 217, 110,  64, 118, 162, 123, 230, 126,
+  255, 127, 230, 126, 162, 123,  64, 118, 217, 110, 139, 101,
+  129,  90, 235,  77, 255,  63, 251,  48,  32,  33, 180,  16,
+    0,   0,  76, 239, 224, 222,   5, 207,   1, 192,  21, 178,
+  127, 165, 117, 154,  39, 145, 192, 137,  94, 132,  26, 129,
+    1, 128,  26, 129,  94, 132, 192, 137,  39, 145, 117, 154,
+  127, 165,  21, 178,   1, 192,   5, 207, 224, 222,  76, 239
+];
+    const TIMER_MICRO_SECONDS: u32 = 125;
 
     /// This allocation is shared across all USB endpoints. It needs to be large
     /// enough to hold the maximum packet size for *all* endpoints. If you start
@@ -47,14 +52,16 @@ mod app {
 
     #[local]
     struct Local {
-        class: AudioClass<'static, Bus>,
         usb_device: UsbDevice<'static, Bus>,
         led: board::Led,
-        sine_data: [u8; 96],
+        timer: hal::pit::Pit<0>,
     }
 
     #[shared]
-    struct Shared {}
+    struct Shared {
+        usb_audio_on: bool,
+        usb_audio: AudioClass<'static, Bus>,
+    }
 
     #[init(local = [bus: Option<UsbBusAllocator<Bus>> = None])]
     fn init(ctx: init::Context) -> (Shared, Local) {
@@ -62,9 +69,13 @@ mod app {
             mut gpio2,
             pins,
             usb,
+            pit: (mut timer, _, _, _),
             ..
         } = my_board(ctx.device);
 
+        timer.set_load_timer_value(TIMER_MICRO_SECONDS);
+        timer.set_interrupt_enable(true);
+        timer.enable();
         let led = board::led(&mut gpio2, pins.p13);
 
         let bus = BusAdapter::with_speed(usb, &EP_MEMORY, &EP_STATE, SPEED);
@@ -79,22 +90,13 @@ mod app {
                 StreamConfig::new_discrete(Format::S16le, 1, &[48_000], TerminalType::InMicrophone)
                     .unwrap(),
             )
-            .output(
-                StreamConfig::new_discrete(
-                    Format::S24le,
-                    2,
-                    &[44100, 48000, 96000],
-                    TerminalType::OutSpeaker,
-                )
-                .unwrap(),
-            )
             .build(usb_bus)
             .unwrap();
 
         // Create USB device
         let string_descriptor = StringDescriptors::new(LangID::EN_US)
             .manufacturer("Hammernet")
-            .product("Audio Port - Teensy 4")
+            .product("Audio Port - Teensy 4 - Pure micro")
             .serial_number("42");
 
         let usb_device = UsbDeviceBuilder::new(usb_bus, VID_PID)
@@ -104,66 +106,78 @@ mod app {
             .unwrap()
             .build();
 
-        let sine_data = unsafe { *(&SINETAB as *const _ as *const [u8; 96]) };
         (
-            Shared {},
+            Shared {
+                usb_audio: class,
+                usb_audio_on: false,
+            },
             Local {
-                class,
                 led,
                 usb_device,
-                sine_data,
+                timer,
             },
         )
     }
 
-    #[task(binds = USB_OTG1, local = [usb_device, class, led, sine_data, configured: bool = false, write_count: usize = 0], priority = 2)]
-    fn usb1(ctx: usb1::Context) {
+    #[task(binds = PIT, local = [timer, read_count: usize = 0], shared = [usb_audio, usb_audio_on])]
+    fn pit_interrupt(mut ctx: pit_interrupt::Context) {
+        let pit_interrupt::LocalResources {
+            read_count,
+            timer,
+            ..
+        } = ctx.local;
+        while timer.is_elapsed() {
+            timer.clear_elapsed();
+        }
+        let is_audio_on = ctx.shared.usb_audio_on.lock(|x| *x);
+        if !is_audio_on {
+            return;
+        }
+        *read_count += 1;
+        if *read_count >= 8 {
+            *read_count = 0;
+        }
+        // take one 8th of the sine wave
+        let start = *read_count * 12;
+        let slice = &SINETAB[start..start + 12];
+        ctx.shared.usb_audio.lock(|audio| audio.write(slice).ok());
+
+    }
+
+    #[task(
+        binds = USB_OTG1, 
+        local = [usb_device, led, configured: bool = false], 
+        shared = [usb_audio, usb_audio_on], 
+        priority = 1
+    )]
+    fn usb1(mut ctx: usb1::Context) {
         let usb1::LocalResources {
-            class,
             usb_device,
             led,
             configured,
-            sine_data,
-            write_count,
             ..
         } = ctx.local;
-        *write_count += 1;
-        if *write_count >= 5 {
-            *write_count = 0;
-            led.toggle();
-        }
-        let input_alt = class.input_alt_setting().unwrap();
-        let output_alt = class.output_alt_setting().unwrap();
-        //if device.state() == UsbDeviceState::Configured {}
-        if usb_device.poll(&mut [class as &mut dyn usb_device::class::UsbClass<Bus>]) {
-            let mut buf = [0u8; 1024];
-            if output_alt > 0 && class.read(&mut buf).is_ok() {}
-        }
-
-        if usb_device.state() == UsbDeviceState::Configured {
-            if !*configured {
-                usb_device.bus().configure();
+        ctx.shared.usb_audio.lock(|usb_audio| {
+            let (_, _, request_result) =  usb_device.poll_specific(&mut [usb_audio]);
+            if *configured {
+                if let Some(request) = request_result  {
+                // volume is turned on or off
+                    if *configured && request.request == 11u8 {
+                        let value = request.value;
+                        let is_on = value == 1u16;
+                        ctx.shared.usb_audio_on.lock(|x| *x = is_on);
+                    }
+                }
             }
-            *configured = true;
-            //led.toggle();
-        } else {
-            *configured = false;
-        }
-        if input_alt > 0 {
-            class.write(sine_data).ok();
-        }
-
-        //// Handle USB configuration state
-        //if device.state() == UsbDeviceState::Configured {
-        //    if !*configured {
-        //        device.bus().configure();
-        //        *configured = true;
-        //        led.toggle(); // Visual indicator of configuration
-        //    }
-        //    // Only write data when configured
-        //    class.write(sine_data).ok();
-        //} else {
-        //    *configured = false;
-        //}
+            if usb_device.state() == UsbDeviceState::Configured {
+                if !*configured {
+                    usb_device.bus().configure();
+                    led.toggle();
+                }
+                *configured = true;
+            } else {
+                *configured = false;
+            }
+        });
     }
 }
